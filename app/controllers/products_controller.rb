f311a18145ae992e0d462581ddbcf6a7c0c5a269
Todd Pangilinan <pang.todd@gmail.com>
2022-06-14 23:19:48 -0700

    update product and review controllers to restrict access based on authorizaion.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :only => [:new, :edit, :destroy] do
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
 
   def index
     @products = Product.all
